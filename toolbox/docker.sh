2f8b25197551f305039161646fe3b51891ed4c44
Alan B <a.brevick@techsmith.com>
2024-04-02 11:11:34 -0400

    add buildx

--- toolbox/docker.sh
+++ toolbox/docker.sh
@@ -10,15 +10,26 @@ CMD=""
 #ARGS="-v $PWD:/mkdocs -v "${docs_path}":/docs"
 DOCKERHUB_USER=defenestration
 
+# on M3 mac, the linux/arm64/v8 platform should work.
+
+buildx() {
+echo '> buildx'
+# start buildx container
+docker buildx create --name buildx
+login_dockerhub && docker buildx build . --platform linux/amd64,linux/aarch64,linux/arm64/v8 --cache-from="${DOCKERHUB_USER}/${IMAGENAME}" --tag ${DOCKERHUB_USER}/${IMAGENAME}:${TAG} --builder buildx --push
+docker buildx rm buildx
+}
+
 build() {
 echo '> build'
 docker build . --platform linux/amd64 --tag ${IMAGENAME}:${TAG} --tag ${IMAGENAME}:${TAG}_amd64 && \
 docker image ls ${IMAGENAME}:${TAG}
+build_arm64
 }
 
 build_arm64() {
 echo '> build_arm64'
-docker build . --platform darwin/arm64 --tag ${IMAGENAME}:${TAG}_arm64
+docker build . --platform linux/arm64/v8 --tag ${IMAGENAME}:${TAG}_arm64
 docker image ls ${IMAGENAME}:${TAG}_arm64
 }
 
@@ -49,12 +60,14 @@ shell
 
 push_dockerhub() {
 echo '> push to dockerhub'
-docker tag ${IMAGENAME}:${TAG} ${DOCKERHUB_USER}/${IMAGENAME}:${TAG}
-docker push ${DOCKERHUB_USER}/${IMAGENAME}:${TAG}
+#docker tag ${IMAGENAME}:${TAG} ${DOCKERHUB_USER}/${IMAGENAME}:${TAG}
+#docker push ${DOCKERHUB_USER}/${IMAGENAME}:${TAG}
+buildx
 }
 
 login_dockerhub(){
-docker login docker.io -u ${DOCKERHUB_USER}
+#docker login docker.io -u ${DOCKERHUB_USER}
+docker login
 }
 
 #if [[ $1 == "build" || $1 == "run"  || $1 == "shell" || $1 == "validate" ]] ; then
